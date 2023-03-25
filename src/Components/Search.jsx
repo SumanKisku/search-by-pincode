7bb041c891f1895e580b6a7f78989ed748bef373
Suman Kisku <sumankisku1@gmail.com>
2023-03-26 00:59:41 +0530

    cleanup, styling, animation

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 // React toastify
@@ -50,18 +50,19 @@ const Search = () => {
   }
 
   return (
-    <div className="mx-5">
+    <div className="m-5">
       <p className="font-black text-xl">Enter Pincode</p>
       <form className="flex flex-col">
-        <input onChange={handleInput} type="number" name="pincode" id="pincode" placeholder="Pincode" className="border-2 border-gray-800 p-1 px-2 rounded-lg appearance-none" />
-        <button className="text-white flex  bg-orange-600 py-2 px-6 rounded-2xl mt-2 cursor-pointer w-min disabled:opacity-75 disabled:bg-slate-500" onClick={handleClick} disabled={isLoading}>
-          {isLoading 
-          ? 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-spin w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-          </svg>
-          : 
-          "Lookup"}
+        <input onChange={handleInput} type="number" name="pincode" id="pincode" placeholder="Pincode" className="border-2 border-gray-800 p-1 px-2 rounded-lg appearance-none ease-in-out duration-300 hover:shadow-md focus:outline-orange-600" />
+        <button className="text-white flex  bg-orange-600 py-2 px-6 rounded-2xl mt-2 cursor-pointer w-min disabled:opacity-75 disabled:bg-slate-500 ease-in-out duration-300 hover:shadow-lg hover:scale-110" onClick={handleClick} disabled={isLoading}>
+          {isLoading
+            ?
+            <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            :
+            "Lookup"}
         </button>
       </form>
       {/* Loader animation */}
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,32 +1,44 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const SearchResults = () => {
   const [locations, setLocations] = useState(JSON.parse(sessionStorage.getItem("locations")));
+  const [filteredLocations, setFilteredLocations] = useState(locations);
 
-  useEffect(() => {
-    console.log(locations);
-  }, [])
+
+  function handleChange(e) {
+    let searchValue = e.target.value;
+    setFilteredLocations(() => (
+      locations.filter((location) => {
+        return location["Name"].toLowerCase().includes(searchValue) || location["District"].toLowerCase().includes(searchValue);
+      })
+    ))
+  }
+
   return (
-    <div className="mx-5">
+    <div className="m-5">
       <p className="font-black text-xl">Pincode: {locations[0]["Pincode"]}</p>
       <form className="flex flex-col">
-        <input type="text" name="filter" id="filter" placeholder="Filter" className="border-2 border-gray-800 p-1 px-2 rounded-lg appearance-none" />
+        <input onChange={handleChange} type="text" name="filter" id="filter" placeholder="Filter" className="border-2 border-gray-800 p-1 px-2 rounded-lg appearance-none ease-in-out duration-300 hover:shadow-md focus:outline-orange-600" />
       </form>
 
-      <div className="flex flex-wrap justify-center gap-2 mt-10">
+      <div id="locations" className="flex flex-wrap justify-center gap-2 mt-10">
 
         {
-          locations.map((location) => {
+          filteredLocations.length > 0
+          ?
+          filteredLocations.map((location) => {
             return (
-              <div className="border w-64">
-                <p>Name: {location["Name"]}</p>
-                <p>Branch Type: { }</p>
-                <p>Delivery status: {location["DeliveryStauts"]}</p>
-                <p>District: {location["District"]}</p>
-                <p>Division: {location["Division"]}</p>
+              <div key={location["Name"]} className="border w-64 p-2 rounded-lg shadow-sm hover:shadow-lg ease-out duration-300">
+                <p><span className="font-semibold">Name:</span> {location["Name"]}</p>
+                <p><span className="font-semibold">Branch</span> Type: {location["BranchType"]}</p>
+                <p><span className="font-semibold">Delivery status:</span> {location["DeliveryStatus"]}</p>
+                <p><span className="font-semibold">District:</span> {location["District"]}</p>
+                <p><span className="font-semibold">Division:</span> {location["Division"]}</p>
               </div>
             )
           })
+          :
+          <p>Couldn’t find the postal data you’re looking for…</p>
         }
       </div>
     </div>
